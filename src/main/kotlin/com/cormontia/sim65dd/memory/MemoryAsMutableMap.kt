6c48bf6db310172d938a5824c61e5419c5484881
Serge <nerostjust@gmail.com>
2024-02-12 20:54:38 +0100

    Implement MemoryAsMutableMap.

--- src/main/kotlin/com/cormontia/sim65dd/memory/MemoryAsMutableMap.kt
+++ src/main/kotlin/com/cormontia/sim65dd/memory/MemoryAsMutableMap.kt
@@ -5,6 +5,54 @@ import com.cormontia.sim65dd.CentralProcessingUnit
 class MemoryAsMutableMap: Memory {
     private val map = mutableMapOf<UShort, UByte>()
 
+    // Auxiliary functions.
+
+    override operator fun get(location: UShort): UByte {
+        return map.getOrDefault(location, 0u)
+    }
+
+    override operator fun set(location: UShort, value: UByte) {
+        map[location] = value
+    }
+
+    private fun getter8bit(
+        cpu: CentralProcessingUnit,
+        zeroPageLocation: UByte,
+        getterFunction: (CentralProcessingUnit, UByte) -> UByte,
+    ): UByte {
+        val location = getterFunction(cpu, zeroPageLocation)
+        return map.getOrPut(location.toUShort()) { 0u }
+    }
+    private fun setter8bit(
+        cpu: CentralProcessingUnit,
+        zeroPageLocation: UByte,
+        value: UByte, setterFunction: (CentralProcessingUnit, UByte) -> UByte,
+    ) {
+        val location = setterFunction(cpu, zeroPageLocation)
+        map[location.toUShort()] = value
+    }
+    private fun getter16bit(
+        cpu: CentralProcessingUnit,
+        lsb: UByte,
+        msb: UByte,
+        locationCalculator: (CentralProcessingUnit, UByte, UByte) -> UShort,
+    ): UByte {
+        val location = locationCalculator(cpu, lsb, msb)
+        return map.getOrPut(location) { 0u }
+    }
+    private fun setter16bit(
+        cpu: CentralProcessingUnit,
+        lsb: UByte,
+        msb: UByte,
+        value: UByte,
+        locationCalculator: (CentralProcessingUnit, UByte, UByte) -> UShort,
+    ) {
+        val location = locationCalculator(cpu, lsb, msb)
+        map[location] = value
+    }
+
+    // interface implementation.
+
     override fun getAbsolute(lsb: UByte, msb: UByte): UByte {
         val location = absolute(lsb, msb)
         val value = map.getOrPut(location) { 0u }
@@ -16,68 +64,53 @@ class MemoryAsMutableMap: Memory {
         map[location] = value
     }
 
-    override fun getAbsoluteX(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getAbsoluteX(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte) =
+        getter16bit(cpu, lsb, msb, ::absoluteX)
 
     override fun setAbsoluteX(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter16bit(cpu, lsb, msb, value, ::absoluteX)
     }
 
-    override fun getAbsoluteY(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getAbsoluteY(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte) =
+        getter16bit(cpu, lsb, msb, ::absoluteY)
 
     override fun setAbsoluteY(cpu: CentralProcessingUnit, lsb: UByte, msb: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter16bit(cpu, lsb, msb, value, ::absoluteY)
     }
 
     override fun getZeroPage(location: UByte): UByte {
-        TODO("Not yet implemented")
+        return map.getOrPut(location.toUShort()) { 0u }
     }
 
     override fun setZeroPage(location: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        map[location.toUShort()] = value
     }
 
-    override fun getZeroPageX(cpu: CentralProcessingUnit, location: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getZeroPageX(cpu: CentralProcessingUnit, location: UByte) =
+        getter8bit(cpu, location, ::zeroPageX)
 
     override fun setZeroPageX(cpu: CentralProcessingUnit, location: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter8bit(cpu, location, value, ::zeroPageX)
     }
 
-    override fun getZeroPageY(cpu: CentralProcessingUnit, location: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getZeroPageY(cpu: CentralProcessingUnit, location: UByte) =
+        getter8bit(cpu, location, ::zeroPageY)
 
     override fun setZeroPageY(cpu: CentralProcessingUnit, location: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter8bit(cpu, location, value, ::zeroPageY)
     }
 
-    override fun getIndirectIndexedY(cpu: CentralProcessingUnit, location: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getIndirectIndexedY(cpu: CentralProcessingUnit, location: UByte): UByte =
+        getter8bit(cpu, location, ::getIndirectIndexedY)
 
     override fun setIndirectIndexedY(cpu: CentralProcessingUnit, location: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter8bit(cpu, location, value, ::getIndirectIndexedY)
     }
 
-    override fun getIndexedIndirectX(cpu: CentralProcessingUnit, location: UByte): UByte {
-        TODO("Not yet implemented")
-    }
+    override fun getIndexedIndirectX(cpu: CentralProcessingUnit, location: UByte): UByte =
+        getter8bit(cpu, location, ::getIndexedIndirectX)
 
     override fun setIndexedIndirectX(cpu: CentralProcessingUnit, location: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        setter8bit(cpu, location, value, ::getIndexedIndirectX)
     }
-
-    override operator fun get(location: UShort): UByte {
-        return map.getOrDefault(location, 0u)
-    }
-
-    override operator fun set(location: UShort, value: UByte) {
-        map[location] = value
-    }
-
 }
